55ad5e45cd145a1ac297766ab7c9bc685179e242
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-09-19 03:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+var players = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+function theBeatlesPlay(players, instruments) {
+  var allPlayers = [];
+  for (var i = 0; i < players.length; i++) {
+    allPlayers.push(players[i] + " plays " + instruments[i]);
+  }
+  return allPlayers;
+}
+
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while(i < facts.length) {
+    newFacts.push(facts[i] + "!!!");
+  }
+  return newFacts;
+}
